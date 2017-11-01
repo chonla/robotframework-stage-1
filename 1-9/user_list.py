19fe525b10ca0e96b3fc9bf286c023b405e1da00
Chonlasith Jucksriporn <chonlasith@gmail.com>
2017-11-01 21:06:55 +0700

    Add some variable examples into user_list.py

--- 1-9/user_list.py
+++ 1-9/user_list.py
@@ -1,2 +1,7 @@
 VALID_USERNAME = "demouser"
 VALID_PASSWORD = "demopassword"
+# ตัวอย่างตัวแปรอื่น ๆ
+INTEGER = 42
+STRINGS = ["one", "two", "kolme", "four"]
+NUMBERS = [1, INTEGER, 3.14]
+MAPPING = {"one": 1, "two": 2, "three": 3}
